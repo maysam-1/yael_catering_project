129076ab726fd17a602e4c8993f1f3b76d73f7b9
Karen7-salih <Karen7-salih.salihkaren345@gmail.com>
2025-03-19 15:24:05 +0200

    removed backend from main

--- backend/src/schemas/user_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class UserSchema(BaseModel):
-    id: Optional[int] = None
-    username: str
-    password: str
-    role_id: Optional[int] = None
