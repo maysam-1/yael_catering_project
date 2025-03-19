398614b1508b41cce5dc21989f2a5ff2257f77e8
maysam-1 <maysamkh44@gmail.com>
2025-03-19 15:58:23 +0200

    added saparate error handling for username and password in login

--- backend/src/services/user_service.py
+++ backend/src/services/user_service.py
@@ -3,7 +3,11 @@ from fastapi import HTTPException
 from ..models.user_model import User
 
 def login(db: Session, username: str, password: str):
-    user = db.query(User).filter(User.username == username and User.password == password).first()
+    user = db.query(User).filter(User.username == username).first()
     if user is None:
-        raise HTTPException(status_code = 401, detail = "Incorrect username or password.")
+        raise HTTPException(status_code=404, detail="User not found.")  # 404 for incorrect username
+
+    if user.password != password:
+        raise HTTPException(status_code=401, detail="Incorrect password.")  # 401 for wrong password
+
     return user
